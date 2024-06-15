dad36d20c9e4bedccb5b47c896d5ee18ac1f9cff
szqhan <59555166+szqhan@users.noreply.github.com>
2024-06-15 12:31:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git qhan https://github.com/szqhan/v2ray-server.git' >> feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
